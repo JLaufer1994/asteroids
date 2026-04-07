57ab0d6d8d1dec5d5350a60ae7141671504cca7d
Jesse Laufer <jss.laufer@gmail.com>
2026-04-06 22:46:40 -0400

    FPS

--- main.py
+++ main.py
@@ -9,12 +9,20 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
-        log_state()
         for event in pygame.event.get():
-            pass    
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                return
         screen.fill("black")
         pygame.display.flip()
+        clock_time = clock.tick(60)
+        dt = clock_time/1000
+        log_state()
+        
 
 
 if __name__ == "__main__":
