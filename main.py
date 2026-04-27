d0b66742706b46b978f0311f0566e001d1f6c902
Jesse Laufer <jss.laufer@gmail.com>
2026-04-27 09:51:44 -0400

    Add player class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
@@ -11,13 +12,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 return
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         clock_time = clock.tick(60)
         dt = clock_time/1000
